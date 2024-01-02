e3afc6a359588aa065ecb59618db1e8c27a1485d
nine-hundred <ninehundred@outlook.kr>
2024-01-03 01:13:41 +0900

    feat: benchmark code

--- benchmark_test.go
+++ benchmark_test.go
@@ -2,13 +2,13 @@ package benchmark
 
 import (
 	"bytes"
+	"io"
 	"net/http"
 	"testing"
 )
 
 func BenchmarkFramework(b *testing.B) {
 	b.Run("get", func(b *testing.B) {
-		b.ReportAllocs()
 		url := "http://localhost:8080/item?id=1"
 		for i := 0; i < b.N; i++ {
 			resp, err := http.Get(url)
@@ -21,10 +21,10 @@ func BenchmarkFramework(b *testing.B) {
 				b.Fatalf("HTTP status code is not 200: %d", resp.StatusCode)
 			}
 		}
+		b.ReportAllocs()
 	})
 
 	b.Run("post", func(b *testing.B) {
-		b.ReportAllocs()
 		url := "http://localhost:8080/item"
 		body := []byte(`{"id": 2, "name": "new item"}`)
 		for i := 0; i < b.N; i++ {
@@ -33,6 +33,7 @@ func BenchmarkFramework(b *testing.B) {
 				panic(err)
 			}
 			req.Header.Set("header", "header")
+			req.Header.Set("Content-Type", "application/json")
 
 			client := &http.Client{}
 			resp, err := client.Do(req)
@@ -42,8 +43,10 @@ func BenchmarkFramework(b *testing.B) {
 
 			defer resp.Body.Close()
 			if resp.StatusCode != 200 {
-				b.Fatalf("HTTP status code is not 200: %d", resp.StatusCode)
+				body, _ := io.ReadAll(resp.Body)
+				b.Fatalf("HTTP status code is not 200: %d, reason: %s", resp.StatusCode, string(body))
 			}
 		}
+		b.ReportAllocs()
 	})
 }
